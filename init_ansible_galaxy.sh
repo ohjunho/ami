a0016db1ffefaaf878f0d8f2a9a266f865a6e3c5
ohjunho <oh.junho@gmail.com>
2020-03-16 11:34:59 +0900

    Toolchain Nexus 설치 및 구성
    [Resolution]
    ansible-galaxy 선행 작업 수행용 shell
    
    [DoD]
    
    [Team]
    Automation
    
    [Company]
    BespinGlobal

---
+++ init_ansible_galaxy.sh
@@ -0,0 +1,8 @@
+# !/bin/bash
+
+ansible-galaxy install lean_delivery.java
+ansible-galaxy install savoirfairelinux.nexus3-oss
+ansible-galaxy install gantsign.maven
+ansible-galaxy install geerlingguy.jenkins
+
+
