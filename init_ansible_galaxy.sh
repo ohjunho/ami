5da166c26410af16edeb7ab65ed26b2ed78e45d8
simplesK <namlyong@gmail.com>
2020-03-18 10:38:40 +0900

    [Resolution] 선행 작업 CMSA-303 Toolchain Ansible + packer 프로젝트 구성 : java8 특이사항 : 아래 문법 선 수행 후 작업 필요 ansible-galaxy install geerlingguy.gitlab
    [DoD] AMI 이미지를 통해 EC2 인스턴스 생성.
    Web을 통해 gitlab 관리페이지 접근 가능 여부 확인
    
    Security Group 설정에서 80, 443 port 허용 필요
    -> https://{ec2-hostname}
    
    [Team] Automation
    [Company] BespinGlobal

--- init_ansible_galaxy.sh
+++ init_ansible_galaxy.sh
@@ -1,8 +1,7 @@
 # !/bin/bash
 
+ansible-galaxy install geerlingguy.gitlab
 ansible-galaxy install lean_delivery.java
 ansible-galaxy install savoirfairelinux.nexus3-oss
 ansible-galaxy install gantsign.maven
 ansible-galaxy install geerlingguy.jenkins
-
-
